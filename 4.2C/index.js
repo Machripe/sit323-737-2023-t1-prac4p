a057bf2647ad4106d35e946fa6636e9082762d8e
Matthew Pettit <mcpettit@deakin.edu.au>
2023-04-03 15:06:11 +1000

    Included passport in 4.2 and error messages

--- 4.2C/index.js
+++ 4.2C/index.js
@@ -2,15 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const passport = require('passport');
+require('./passport.js')(passport);
 const jwt = require('jsonwebtoken');
 
-// Authentication server key (using random secret here instead of static for no particular reason)
-const ACCESS_TOKEN_SECRET = require('crypto').randomBytes(64).toString('hex');
+// Authentication server private key for token generation (tokens to be verified by passport service using paired public key)
+const fs = require('fs');
+const PRIVATE_KEY = fs.readFileSync('./rsa_private.pem', 'utf8');
+const crypto = require('crypto');
 
-// Simulate authentication database
+// Simulate user database with simple access levels
 const authenticationMap = new Map();
-authenticationMap.set("matthew", { password: "abc123", access: 0b1111 });
-authenticationMap.set("christopher", { password: "123456", access: 0b0100 });
+// Create users, encrypt stored passwords, grant access permissions
+function createUser(username, password, access) {
+    let salt = crypto.randomBytes(32).toString('hex');
+    let hashedPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+    authenticationMap.set(username, { hashedPassword: hashedPassword, salt: salt, access: access });
+}
+createUser("matthew", "abc123", 0b1111 ); // Can access all math functions
+createUser("christopher", "123456", 0b0100 ); // Can only access subtraction
 
 // Simulate authorization levels using binary bit flags to allow for easily fine tuning access to individual features
 const authorizationMap = new Map();
@@ -36,38 +46,47 @@ function performMath(req, res, callback) {
     res.status(200).json({ status: 200, type: callback.name, input: [num1, num2], result: result });
 }
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.status(400).json({ status: 400, message: "Authentication Failure: Token not found" });
-
-    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(400).json({ status: 400, message: "Authentication Failure: Invalid token" });
-        req.user = user;
-        next();
-    });
-}
-
 function authorizeAction(req, res, callback) {
     let value = authorizationMap.get(callback.name) & req.user.access; // perform bitwise AND on user access and required access
-    if (value == 0) return res.status(403).json({ status: 403, message: "Unauthorized Action" });
+    if (value == 0) return res.status(403).json({ status: 403, message: "Unauthorized action" });
     performMath(req, res, callback);
 }
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const hashedPassword = crypto.pbkdf2Sync(req.body.password, authenticationMap.get(username).salt, 10000, 64, 'sha512').toString('hex');
     // Not using any password encryption for this task
-    if (authenticationMap.get(username).password !== password) return res.status(401).json({ status: 401, message: "Login Failure: Invalid credentials" });
+    if (hashedPassword !== authenticationMap.get(username).hashedPassword) return res.status(401).json({ status: 401, message: "Login Failure: Invalid credentials" });
 
-    const auth = { name: username, access: authenticationMap.get(username).access };
-    const accessToken = jwt.sign(auth, ACCESS_TOKEN_SECRET)
-    res.status(200).json({ status: 200, accessToken: accessToken });
+    const expiresIn = '90000';
+
+    const payload = {
+        sub: { username, access: authenticationMap.get(username).access }
+    };
+
+    const token = jwt.sign(payload, PRIVATE_KEY, { expiresIn: expiresIn, algorithm: 'RS256' });
+    res.status(200).json({ status: 200, username: username, token: token, expiresIn: expiresIn });
 });
+
+app.use(passport.initialize());
+
+// Authenticates supplied token and provides custom error messages
+function authenticateToken(req, res, next) {
+    passport.authenticate('jwt', { session: false }, (error, user, info) => {
+        if (user == false) {
+            if (info.name === "TokenExpiredError") return res.status(400).json({ status: 400, message: "Authentication Failure: Expired token" });
+            if (info.name === "JsonWebTokenError") return res.status(400).json({ status: 400, message: "Authentication Failure: Invalid token" });
+            if (info.name === "Error") return res.status(400).json({ status: 400, message: "Authentication Failure: Missing token" });
+            return res.status(400).json({ status: 400, message: "Authentication Failure: General" });
+        } 
+        req.user = user;
+        next();
+    })(req, res);
+}
 
 app.post("/add", authenticateToken, (req, res) => { authorizeAction(req, res, addition); })
 app.post("/multiply", authenticateToken, (req, res) => { authorizeAction(req, res, multiplication); })
-app.post("/subtract", authenticateToken, (req, res) => { authorizeAction(req, res, subtraction); })
+app.post("/subtract", authenticateToken, (req, res) => { authorizeAction(req, res, subtraction); });
 app.post("/divide", authenticateToken, (req, res) => { authorizeAction(req, res, division); })
 
 app.use((req, res) => {
